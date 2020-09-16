e9373ccfcd91af2b3de7cf226d795579b368baa3
KVCobbs <kenyacobbs9001@gmail.com>
2020-09-16 16:25:58 -0400

    tired

--- profiles/models.py
+++ profiles/models.py
@@ -9,6 +9,10 @@ class Profile(models.Model):
     class Meta:
         verbose_name_plural = 'profiles'
 
+#I thought about changing this more to me a profile page. And then the user will have
+#The line image_url to continue to have an image as a representation of them and then a seperate profile page
+#It won't show the saved insults and stuff but more to show like a profile of the person. IDK
+
     image_url = models.URLField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
